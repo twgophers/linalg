0308fb87ebb5d6de3ea273040816e9302884c990
Rodrigo Pinheiro <rpinheir@thoughtworks.com>
2017-02-23 08:48:08 -0300

    Adding subtract function

--- vector/vector.go
+++ vector/vector.go
@@ -1,10 +1,30 @@
 package vector
 
-func Add(v1, v2 []float64) []float64 {
-  result := make([]float64, len(v1))
 
-  for i, x := range v1 {
-    result[i] = x + v2[i]
+func Add(v, w []float64) []float64 {
+
+  if (len(v) != len(w)) {
+    panic("The vectors have different sizes.")
   }
+
+  result := make([]float64, len(v))
+
+  for i, x := range v {
+    result[i] = x + w[i]
+  }
+
+  return result
+}
+
+func Subtract(v, w []float64) []float64 {
+  if (len(v) != len(w)) {
+    panic("The vectors have different sizes.")
+  }
+  result := make([]float64, len(v))
+
+  for i, x := range v {
+    result[i] = x - w[i]
+  }
+
   return result
 }
